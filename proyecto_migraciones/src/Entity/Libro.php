83403ef15997531449183317aac925eab10fdc3c
Lydia <124252166+lygarmo@users.noreply.github.com>
2025-03-21 15:10:21 +0100

    Add files via upload

---
+++ proyecto_migraciones/src/Entity/Libro.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LibroRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LibroRepository::class)]
+class Libro
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $titulo = null;
+
+    #[ORM\Column]
+    private ?int $a_publicacion = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $autor = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $editorial = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): static
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getAPublicacion(): ?int
+    {
+        return $this->a_publicacion;
+    }
+
+    public function setAPublicacion(int $a_publicacion): static
+    {
+        $this->a_publicacion = $a_publicacion;
+
+        return $this;
+    }
+
+    public function getAutor(): ?string
+    {
+        return $this->autor;
+    }
+
+    public function setAutor(string $autor): static
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    public function getEditorial(): ?string
+    {
+        return $this->editorial;
+    }
+
+    public function setEditorial(string $editorial): static
+    {
+        $this->editorial = $editorial;
+
+        return $this;
+    }
+}
